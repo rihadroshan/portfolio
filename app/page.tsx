4fae1b7a0826f64d63afb373c3ca5e76409a9d74
Rihad Roshan <152946208+rihadroshan@users.noreply.github.com>
2025-08-15 14:23:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,7 @@ const Portfolio = () => {
               transition={{ duration: 0.5, delay: 0.3 }}
               className="mono text-sm text-gray-500 mb-4 tracking-widest"
             >
-              Securing the Digital World
+              {/*  */}
             </motion.div>
             <motion.h1
               initial={{ opacity: 0, y: 20 }}
